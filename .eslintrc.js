4aa57fcb1838e3671beeaaee9d14791241531c2e
Sumin Sohn <soyw31@gmail.com>
2023-07-06 00:30:20 +0900

    [config] eslint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-var': ['error'], // var 금지
+    'no-console': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn', // 사용하지 않는 변수에 warn
+  },
+};
