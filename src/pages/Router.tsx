ac6fe3ba2a41e7b1562c37f935cbe1f2ce4160b6
Sumin Sohn <soyw31@gmail.com>
2023-07-07 13:26:14 +0900

    경로 오타 수정

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -34,7 +34,7 @@ export const Router = (): React.ReactElement => {
         <Route path='map' element={<MapPage />} />
         <Route path='gathering' element={<Gathering />} />
       </Route>
-      <Route path={staticServerUri + 'detail/:post_idx'} element={<DetailMainPage />} />
+      <Route path={staticServerUri + '/detail/:post_idx'} element={<DetailMainPage />} />
       <Route path={staticServerUri + '/payment'} element={<PaymentPage />} />
       <Route path={staticServerUri + '/review'} element={<ReviewPage />} />
       {/* 상단에 위치하는 경로를 모두 확인, 일치하는 경로가 없는 경우 처리 */}
