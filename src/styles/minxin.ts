b8877e3158772804a6e281a94db996e5f9a4b42a
Sumin Sohn <soyw31@gmail.com>
2023-08-04 22:02:11 +0900

    [feat] 텍스트 스타일 만드는 믹스인 typoStyles 작성

--- src/styles/minxin.ts
+++ src/styles/minxin.ts
@@ -1,20 +1,28 @@
 import { css } from 'styled-components';
 import type { FlattenSimpleInterpolation } from 'styled-components';
 
-export const flexSet = (
-  justify = 'center',
-  align = 'center',
-): FlattenSimpleInterpolation => css`
+export const flexSet = (justify = 'center', align = 'center'): FlattenSimpleInterpolation => css`
   display: flex;
   justify-content: ${justify};
   align-items: ${align};
 `;
 
-export const mediaBpDown = (
-  maxWidth: string,
-  children: FlattenSimpleInterpolation,
-) => css`
+export const mediaBpDown = (maxWidth: string, children: FlattenSimpleInterpolation) => css`
   @media screen and (max-width: ${maxWidth}) {
     ${children}
   }
+`;
+
+interface TypoStyles {
+  fontFamily: string;
+  fontSize: number;
+  fontWeight: number;
+  lineHeight: number;
+}
+
+export const typoStyles = (style: TypoStyles) => css`
+  font-family: ${style.fontFamily};
+  font-size: ${style.fontSize}px;
+  font-weight: ${style.fontWeight};
+  line-height: ${style.lineHeight};
 `;
