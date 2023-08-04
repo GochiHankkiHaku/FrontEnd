a58006f314115e72e9500820ba6925e211d804fd
Sumin Sohn <soyw31@gmail.com>
2023-08-04 23:32:11 +0900

    [refact] 온보딩 페이지 별 정보 변수에 저장, 페이지 정보 유틸 함수 작성

---
+++ src/pages/OnboardingPage/utils/pagesInformation.ts
@@ -0,0 +1,38 @@
+import { PATH } from 'common/constants';
+
+const pagesPerStep = [
+  { url: '/', name: '돌아가기' },
+  {
+    url: `/${PATH.onBoarding}/${PATH.location}`,
+    name: '장소 선택',
+  },
+  {
+    url: `/${PATH.onBoarding}/${PATH.schedule}`,
+    name: '식사 일정',
+  },
+  {
+    url: `/${PATH.onBoarding}/${PATH.memberCount}`,
+    name: '인원수',
+  },
+  {
+    url: `/${PATH.onBoarding}/${PATH.cook}`,
+    name: '요리',
+  },
+  {
+    url: `/${PATH.onBoarding}/${PATH.cost}`,
+    name: '가격',
+  },
+];
+
+export const getCurrentPageStep = (currentLocation: string) =>
+  pagesPerStep.findIndex(({ url }) => url === currentLocation);
+
+export const getPreviousPage = (currentLocation: string) => {
+  const nextPageStep = getCurrentPageStep(currentLocation) + 1;
+  return pagesPerStep[nextPageStep];
+};
+
+export const getNextPageUrl = (currentLocation: string) => {
+  const nextPageStep = getCurrentPageStep(currentLocation) + 1;
+  return pagesPerStep[nextPageStep].url;
+};
