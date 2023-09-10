9d327a59def5b42b43f5bcaedc481041c84cd09f
Sumin Sohn <soyw31@gmail.com>
2023-09-10 22:55:54 +0900

    [design] 검색창에 검색 아이콘 추가

--- src/pages/OnboardingPage/components/Search.tsx
+++ src/pages/OnboardingPage/components/Search.tsx
@@ -1,30 +1,42 @@
-import React from 'react';
 import styled from 'styled-components';
 import { color, typograpy } from 'styles/constants';
 import { typoStyles } from 'styles/minxin';
+import searchIcon from 'assets/icons/search.svg';
 
 export default function Search() {
   return (
     <Wrap>
-      {/* TODO 돋보기 에셋 받고 추가 */}
       <Input type='text' placeholder='레시피 검색' />
     </Wrap>
   );
 }
 
 const Wrap = styled.div`
+  display: flex;
+  gap: 10px;
+
   width: 100%;
   height: 47px;
   padding: 10px 20px;
 
   border-radius: 47px;
   border: 1px solid ${color.main[4]};
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    top: 0;
+    left: 0;
+    background-image: url(${searchIcon});
+    background-position: center;
+    background-size: cover;
+  }
 `;
 
 const Input = styled.input`
-  /* background-color: yellow; */
-
-  width: 100%;
+  flex: 1;
   height: 100%;
 
   ${typoStyles(typograpy.paragraph[3])};
